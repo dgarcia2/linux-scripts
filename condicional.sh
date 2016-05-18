11bf821abd06ca117b3f67c079e957585973582c
Your Name <you@example.com>
2016-05-18 11:06:55 +0200

    nuevo script con if

---
+++ condicional.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo Hola, sé hacer scripts uuUUUuuuuuuu
+
+echo -n "Dame un número: "
+read valor
+
+if [ ${valor} -eq 666 ]
+then
+	echo Es el número de la bestia
+else
+	echo NO es el número mal
+fi
